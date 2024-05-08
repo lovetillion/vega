96ec6e8937ea9506e84a0a74705502a87fa1fa90
thejourker <smartyshirts@icloud.com>
2024-05-08 01:28:39 -0400

    simple example to handle conversion from type 0/2 cashaddr

--- token_addr.js
+++ token_addr.js
@@ -1,19 +1,36 @@
+// based on https://github.com/bitauth/libauth/blob/master/docs/addresses.md
+
 import { decodeCashAddress, stringify, encodeCashAddress, hexToBin } from '@bitauth/libauth';
 
-// eatBCH South Sudan
-const address2 = 'bitcoincash:qrsrvtc95gg8rrag7dge3jlnfs4j9pe0ugrmeml950';
-console.log( address2 )
-const tokenAddress = decodeCashAddress(address2);
-console.log(stringify(tokenAddress));
+const input_addr = process.argv[2];
 
-const publicKeyHash = tokenAddress.payload;
+if ( input_addr )
+{
+	const address2 = input_addr;
+	const tokenAddress = decodeCashAddress(address2);
+	console.log(stringify(tokenAddress));
 
-const acceptsTokens = encodeCashAddress({
-  payload: publicKeyHash,
-  prefix: 'bitcoincash',
-  type: 'p2pkhWithTokens',
-}).address;
-console.log(acceptsTokens);
+	const publicKeyHash = tokenAddress.payload;
 
+	const acceptsBCH = encodeCashAddress({
+	  payload: publicKeyHash,
+	  prefix: 'bitcoincash',
+	  type: 'p2pkh',
+	}).address;
+	console.log(acceptsBCH);
+
+	const acceptsTokens = encodeCashAddress({
+	  payload: publicKeyHash,
+	  prefix: 'bitcoincash',
+	  type: 'p2pkhWithTokens',
+	}).address;
+	console.log(acceptsTokens);
+}
+else
+{
+	console.log( "You must specify an address. Example:\n\nnode token_addr.js bitcoincash:ADDRESS" );
+}
+
+// Test - eatBCH South Sudan Address
 // bitcoincash:qrsrvtc95gg8rrag7dge3jlnfs4j9pe0ugrmeml950
 // bitcoincash:zrsrvtc95gg8rrag7dge3jlnfs4j9pe0ugy3293rtu
